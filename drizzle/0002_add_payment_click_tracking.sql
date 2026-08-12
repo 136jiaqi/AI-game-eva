3781600bbe62f21f62c2f0f0bc63252761ab7c5b
136jiaqi <lijiaqi13648060@gmail.com>
2026-08-12 14:19:13 +0800

    清空旧代码

--- drizzle/0002_add_payment_click_tracking.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `evaluation_records` ADD COLUMN `payment_clicked` integer DEFAULT false NOT NULL;
---> statement-breakpoint
-ALTER TABLE `evaluation_records` ADD COLUMN `payment_clicked_at` text DEFAULT '' NOT NULL;
